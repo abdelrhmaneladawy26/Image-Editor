9d0956487a331377facbe6757a68bb3b75d6746e
abdelrhmaneladawy26git config --global user.name abdelrhmaneladawy26git config --global user.name abdelrhmaneladawy26 <abdelrhmaneladawy26@gmail.com>
2022-06-17 04:28:09 +0200

    update files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let saturate = document.getElementById("saturate");
+let contrast = document.getElementById("contrast");
+let brightness = document.getElementById("brightness");
+let sepia = document.getElementById("sepia");
+let grayscale = document.getElementById("grayscale");
+let blur = document.getElementById("blur");
+let hueRotate = document.getElementById("hue-rotate");
